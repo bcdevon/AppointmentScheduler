48a7ed540227a6d87645f264f38d14c045db2630
BradyDevon <bdevon2@wgu.edu>
2023-09-01 18:23:33 -0600

    connection finished for now

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -10,7 +10,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("MainView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../View/MainView.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
