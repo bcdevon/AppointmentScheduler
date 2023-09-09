bd4c9542aad2c72541dfdcbefcd5a82e88544368
BradyDevon <bdevon2@wgu.edu>
2023-09-09 12:57:06 -0600

    cancel button for customer

--- src/Controller/update_CustomerController.java
+++ src/Controller/update_CustomerController.java
@@ -1,4 +1,11 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+
 public class update_CustomerController {
+    public void onSave(ActionEvent actionEvent) {
+    }
+
+    public void onCancel(ActionEvent actionEvent) {
+    }
 }
