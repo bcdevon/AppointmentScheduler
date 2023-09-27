69af1a2a680fdfa66754c1242c2384ca599c01b3
BradyDevon <bdevon2@wgu.edu>
2023-09-26 22:06:33 -0600

    populate customer and appt tables

--- src/DAO/CustomerDAO.java
+++ src/DAO/CustomerDAO.java
@@ -1,4 +1,30 @@
 package DAO;
+import Model.Customer;
+import helper.JDBC;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CustomerDAO {
+    public List<Customer> getAllCustomers(){
+        List<Customer> customers = new ArrayList<>();
+        try {
+            PreparedStatement preparedStatement = JDBC.connection.prepareStatement("SELECT * From customers");
+            String sql = "SELECT * From customers";
+            ResultSet resultSet = preparedStatement.executeQuery(sql);
+            while (resultSet.next()) {
+                Customer customer = new Customer();
+                customer.setId(resultSet.getInt("Customer_ID"));
+                customer.setName(resultSet.getString("Customer_Name"));
+                customer.setAddress(resultSet.getString("Address"));
+                customer.setPostal(resultSet.getString("Postal_Code"));
+                customer.setPhone(resultSet.getString("Phone"));
+                customers.add(customer);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return customers;
+    }
 }
--- src/Model/Customer.java
+++ src/Model/Customer.java
@@ -1,4 +1,57 @@
 package Model;
 
 public class Customer {
+    private int id;
+    private String name;
+    private String address;
+    private String postal;
+    private String phone;
+
+    public Customer() {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.postal = postal;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPostal() {
+        return postal;
+    }
+
+    public void setPostal(String postal) {
+        this.postal = postal;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
