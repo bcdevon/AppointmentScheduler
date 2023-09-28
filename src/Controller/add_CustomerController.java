3f6737696aecb5e46860517530d51b1c6d89f025
BradyDevon <bdevon2@wgu.edu>
2023-09-27 21:45:10 -0600

    add customers not working yet

--- src/Controller/add_CustomerController.java
+++ src/Controller/add_CustomerController.java
@@ -1,16 +1,47 @@
 package Controller;
 
+import Model.Customer;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class add_CustomerController {
-    public void onSave(ActionEvent actionEvent) {
+public class add_CustomerController implements Initializable {
+    public TextField addIDTF;
+    public TextField addNameTF;
+    public TextField addAddressTF;
+    public TextField addPostalCodeTF;
+    public TextField addPhoneTF;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    public void onSave(ActionEvent actionEvent) throws IOException {
+        int idS = Integer.parseInt(addIDTF.getText());
+        String nameS = addNameTF.getText();
+        String addressS = addAddressTF.getText();
+        String postalcodeS = addPostalCodeTF.getText();
+        String phoneS = addPhoneTF.getText();
+        Customer addcustomer = new Customer(idS,nameS,addressS,postalcodeS,phoneS);
+        CustomerTable.add(addcustomer);
+
+        Parent appointment_parent = FXMLLoader.load(getClass().getResource("../View/Appointments.fxml"));
+        Scene appointment_scene = new Scene(appointment_parent);
+
+        //Get the current window and set the scene to the appointment scene
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(appointment_scene);
+        stage.show();
     }
 
     public void onCancel(ActionEvent actionEvent) throws IOException {
