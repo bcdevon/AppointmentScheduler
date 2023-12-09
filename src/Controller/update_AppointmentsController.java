40b0450c6832fcb32470192cf1dc40b17152d686
unknown <brady.devon0@gmail.com>
2023-12-08 19:42:59 -0700

    appointent outside business hours alert

--- src/Controller/update_AppointmentsController.java
+++ src/Controller/update_AppointmentsController.java
@@ -11,10 +11,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -45,13 +42,8 @@ public class update_AppointmentsController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        updateStartTimeComboBox.setItems(FXCollections.observableArrayList("01:00", "01:15","01:30","01:45", "02:00","02:15", "02:30", "02:45", "03:00", "03:15", "03:30", "03:45", "04:00", "04:15", "04:30", "04:45", "05:00", "05:15", "05:30", "05:45",  "06:00", "06:15", "06:30", "06:45", "07:00", "07:15", "07:30", "07:45","08:00", "08:15", "08:30", "08:45", "09:00", "09:15", "09:30", "09:45","10:00", "10:15", "10:30", "10:45", "11:00", "11:15", "11:30", "11:45", "12:00", "12:15", "12:30", "12:45", "13:00","13:15", "13:30", "13:45", "14:00", "14:15", "14:30", "14:45", "15:00", "15:15", "15:30", "15:45", "16:00", "16:15", "16:30", "16:45", "17:00", "17:15", "17:30", "17:45", "18:00", "18:15", "18:30", "18:45", "19:00", "19:15", "19:30", "19:45", "20:00", "20:15", "20:30", "20:45","21:00", "21:15", "21:30", "21:45", "22:00", "22:15", "22:30", "22:45", "23:00", "23:15", "23:30", "23:45", "24:00", "24:15", "24:30", "24:45"));
-        updateEndTimeComboBox.setItems(FXCollections.observableArrayList("01:00", "01:15","01:30","01:45", "02:00","02:15", "02:30", "02:45", "03:00", "03:15", "03:30", "03:45", "04:00", "04:15", "04:30", "04:45", "05:00", "05:15", "05:30", "05:45",  "06:00", "06:15", "06:30", "06:45", "07:00", "07:15", "07:30", "07:45","08:00", "08:15", "08:30", "08:45", "09:00", "09:15", "09:30", "09:45","10:00", "10:15", "10:30", "10:45", "11:00", "11:15", "11:30", "11:45", "12:00", "12:15", "12:30", "12:45", "13:00","13:15", "13:30", "13:45", "14:00", "14:15", "14:30", "14:45", "15:00", "15:15", "15:30", "15:45", "16:00", "16:15", "16:30", "16:45", "17:00", "17:15", "17:30", "17:45", "18:00", "18:15", "18:30", "18:45", "19:00", "19:15", "19:30", "19:45", "20:00", "20:15", "20:30", "20:45","21:00", "21:15", "21:30", "21:45", "22:00", "22:15", "22:30", "22:45", "23:00", "23:15", "23:30", "23:45", "24:00", "24:15", "24:30", "24:45"));
-
-
-
-
-
+        updateStartTimeComboBox.setItems(FXCollections.observableArrayList("01:00", "01:15","01:30","01:45", "02:00","02:15", "02:30", "02:45", "03:00", "03:15", "03:30", "03:45", "04:00", "04:15", "04:30", "04:45", "05:00", "05:15", "05:30", "05:45",  "06:00", "06:15", "06:30", "06:45", "07:00", "07:15", "07:30", "07:45","08:00", "08:15", "08:30", "08:45", "09:00", "09:15", "09:30", "09:45","10:00", "10:15", "10:30", "10:45", "11:00", "11:15", "11:30", "11:45", "12:00", "12:15", "12:30", "12:45", "13:00","13:15", "13:30", "13:45", "14:00", "14:15", "14:30", "14:45", "15:00", "15:15", "15:30", "15:45", "16:00", "16:15", "16:30", "16:45", "17:00", "17:15", "17:30", "17:45", "18:00", "18:15", "18:30", "18:45", "19:00", "19:15", "19:30", "19:45", "20:00", "20:15", "20:30", "20:45","21:00", "21:15", "21:30", "21:45", "22:00", "22:15", "22:30", "22:45", "23:00", "23:15", "23:30", "23:45", "24:00"));
+        updateEndTimeComboBox.setItems(FXCollections.observableArrayList("01:00", "01:15","01:30","01:45", "02:00","02:15", "02:30", "02:45", "03:00", "03:15", "03:30", "03:45", "04:00", "04:15", "04:30", "04:45", "05:00", "05:15", "05:30", "05:45",  "06:00", "06:15", "06:30", "06:45", "07:00", "07:15", "07:30", "07:45","08:00", "08:15", "08:30", "08:45", "09:00", "09:15", "09:30", "09:45","10:00", "10:15", "10:30", "10:45", "11:00", "11:15", "11:30", "11:45", "12:00", "12:15", "12:30", "12:45", "13:00","13:15", "13:30", "13:45", "14:00", "14:15", "14:30", "14:45", "15:00", "15:15", "15:30", "15:45", "16:00", "16:15", "16:30", "16:45", "17:00", "17:15", "17:30", "17:45", "18:00", "18:15", "18:30", "18:45", "19:00", "19:15", "19:30", "19:45", "20:00", "20:15", "20:30", "20:45","21:00", "21:15", "21:30", "21:45", "22:00", "22:15", "22:30", "22:45", "23:00", "23:15", "23:30", "23:45", "24:00"));
 
             try {
             updateContactComboBox.setItems(CustomerDAO.getAllContactNames());
@@ -87,10 +79,12 @@ public class update_AppointmentsController implements Initializable {
 //    ZoneId localTimeZone = ZoneId.systemDefault();
 
     public void onSaveUpdate(ActionEvent actionEvent) throws SQLException, IOException {
+        //get current date and time
         LocalDateTime currentDateTime = LocalDateTime.now();
+        //format date and time
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         String formattedDateTime = currentDateTime.format(formatter);
-        //startdate time from text fields
+        //get date time
         String startDate = updateStartDate.getValue().toString();
         String endDate = updateEndDate.getValue().toString();
         String startTime = updateStartTimeComboBox.getValue().toString() + ":00";
@@ -101,9 +95,29 @@ public class update_AppointmentsController implements Initializable {
         // Define your time zone
         ZoneId localTimeZone = ZoneId.systemDefault();
 
+        // Define Eastern Time Zone
+        ZoneId easternTimeZone = ZoneId.of("America/New_York");
+
 // Create ZonedDateTime objects with the local time and your time zone
         ZonedDateTime startZonedDateTime = ZonedDateTime.of(startDateTime, localTimeZone);
         ZonedDateTime endZonedDateTime = ZonedDateTime.of(endDateTime, localTimeZone);
+
+        // Convert to Eastern Time
+        ZonedDateTime startZonedDateTimeEastern = startZonedDateTime.withZoneSameInstant(easternTimeZone);
+        ZonedDateTime endZoneDateTimeEastern = endZonedDateTime.withZoneSameInstant(easternTimeZone);
+
+        // Check if the appointment time is between 8:00 AM and 10:00 PM Eastern Time
+        if ((startZonedDateTimeEastern.getHour() < 8 || startZonedDateTimeEastern.getHour() >= 22) || (endZoneDateTimeEastern.getHour() < 8 || endZoneDateTimeEastern.getHour() >= 22 )) {
+            // Display an error message or handle the case where the time is not within the allowed range
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error Dialog");
+            alert.setContentText("Error: Appointment time must be between 8:00 AM and 10:00 PM Eastern Time.");
+            alert.showAndWait();
+            System.out.println("Error: Appointment time must be between 8:00 AM and 10:00 PM Eastern Time.");
+            return; // Don't proceed with saving the appointment
+        }
+        System.out.println(startZonedDateTimeEastern);
+
 
 // Convert to UTC
         ZonedDateTime startZonedDateTimeUTC = startZonedDateTime.withZoneSameInstant(ZoneOffset.UTC);
