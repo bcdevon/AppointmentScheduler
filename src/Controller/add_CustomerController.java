c54c28f6bd5df420c369887129cd72af5d14dbbe
unknown <brady.devon0@gmail.com>
2023-12-12 20:33:37 -0700

    javadocs add customer controller

--- src/Controller/add_CustomerController.java
+++ src/Controller/add_CustomerController.java
@@ -29,6 +29,9 @@ import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.ResourceBundle;
 
+/**This is the add_CustomerController class.
+ * This class handles adding customers, save or cancel when a customer is added.
+ * It also manages values in text fields and combo boxes*/
 public class add_CustomerController implements Initializable {
     public TextField addIDTF;
     public TextField addNameTF;
@@ -39,6 +42,10 @@ public class add_CustomerController implements Initializable {
     public ComboBox stateBox;
     private int divisionIDS;
 
+    /**This is the initialize method.
+     * This method is call during initialization and sets up the available value options for the combo boxes.
+     * @param url The location of the FXML file
+     * @param resourceBundle Resources used for initialization*/
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         try {
@@ -54,13 +61,18 @@ public class add_CustomerController implements Initializable {
         // Set initial focus on addNameTF
         Platform.runLater(() -> addNameTF.requestFocus());
     }
+
+    /**This is the onSave method.
+     * This is an eventhandler method that is called when the user clicks the save button to save an added Customer.
+     * It gets values from the text fields, and combo Boxes it formats and converts the time and date.
+     * It also gets information about the user who created the customer and the time it was created.
+     * @param actionEvent  The event triggered when the save button is clicked*/
     public void onSave(ActionEvent actionEvent) throws IOException, SQLException {
         //get current date and time
         LocalDateTime currentDateTime = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
         //get input from each text field
-//        int idS = Integer.parseInt(addIDTF.getText());
         String nameS = addNameTF.getText();
         String addressS = addAddressTF.getText();
         String postalS = addPostalCodeTF.getText();
@@ -72,9 +84,6 @@ public class add_CustomerController implements Initializable {
 
         int rowsAffected = CustomerQuery.insert(nameS, addressS, postalS, phoneS, createDateS, createdBy, lastUpdatedS, lastUpdateByS, divisionIDS );
 
-        //get input from each text field
-
-
         Parent appointment_parent = FXMLLoader.load(getClass().getResource("../View/Appointments.fxml"));
         Scene appointment_scene = new Scene(appointment_parent);
 
@@ -84,6 +93,10 @@ public class add_CustomerController implements Initializable {
         stage.show();
     }
 
+    /**This is the onCancel method.
+     * This is an event handler method that is called when the user clicks the cancel button.
+     * The appointment will not be added and the application returns to the appointments screen.
+     * @param actionEvent The event triggered when the cancel button is clicked*/
     public void onCancel(ActionEvent actionEvent) throws IOException {
         Parent appointment_parent = FXMLLoader.load(getClass().getResource("../View/Appointments.fxml"));
         Scene appointment_scene = new Scene(appointment_parent);
@@ -94,10 +107,15 @@ public class add_CustomerController implements Initializable {
         stage.show();
     }
 
+    /**This is the onCountrySelected method.
+     * This is an event handler method that is called when a user selects a country.
+     * It fetches divisions based on the selected country and populates the statebox
+     * with the corresponding divisions.
+     * @param actionEvent The event triggered by selecting the country. */
     public void onCountrySelected(ActionEvent actionEvent) throws SQLException {
         // Get the selected country
         Country selectedCountry = countryBox.getValue();
-
+        //Check if a Country is selected
         if (selectedCountry != null) {
             try {
                 // Fetch divisions based on the selected country
@@ -112,12 +130,17 @@ public class add_CustomerController implements Initializable {
 
     }
 
+    /**This is the onDivisionSelected method.
+     * This is an event handler method that is called when the user selects a division.
+     * It gets the selected division and fetches the corresponding division ID and stores it
+     * in the divisionIDS field.*/
     public void onDivisionSelected(ActionEvent actionEvent) {
+        //Get the selected Divisions
         String selectedDivision = (String) stateBox.getValue();
-
+        //Check if a division is selected
         if (selectedDivision != null) {
             try {
-                //fetch the division id base on the name
+                //fetch the division id based on the selected division's name
                 divisionIDS = CustomerDAO.getDivisionIdByName(selectedDivision);
             } catch (SQLException e) {
                 throw new RuntimeException(e);
