cfef8347b2e1513cbbfd5018062e85a41033e787
unknown <brady.devon0@gmail.com>
2023-12-26 20:11:08 -0700

    User

--- src/Model/User.java
+++ src/Model/User.java
@@ -1,14 +1,19 @@
 package Model;
 
+/**This is the User class.
+ * This class represents a user with a unique ID and username.*/
 public class User {
     private int userID;
     private String username;
 
+    /**This is the User constructor.
+     * It constructs a User with the specified userID and username*/
     public User(int userID, String username){
         this.userID = userID;
         this.username = username;
     }
 
+    //Getter and setter methods for each User field.
     public int getUserID(){
         return  userID;
     }
